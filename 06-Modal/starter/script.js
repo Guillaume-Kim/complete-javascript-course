df4651596c0471322102dc26043c09d45e838c94
kkiihun <mydodream87@gmail.com>
2024-04-30 21:19:07 +0900

    update

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -6,11 +6,23 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-console.log(btnsOpenModal[i].addEventListener('click', function() {
+const openModal = function () {
     console.log('Button clicked');
     modal.classList.remove('hidden')
     overlay.classList.remove('hidden');
+}
 
-    modal.computedStyleMap.display = 'block';
-}));
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function() {
+    console.log('A Key was pressed');
+});
